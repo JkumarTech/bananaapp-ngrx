2a558c385b766e914903e61972b120c6e65bb32b
Jayakumar Rajendran <jkumar.tech@gmail.com>
2021-01-10 16:14:05 -0600

    Add Banana component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  
 }
